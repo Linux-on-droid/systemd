77008354101e495d8caaa2c99feb7a17e81777c2
Luka Panio <lukapanio@gmail.com>
2024-06-06 20:35:05 +0200

    core: main: Do not wait 30s if using cgroup v1

--- src/core/main.c
+++ src/core/main.c
@@ -3110,8 +3110,7 @@ int main(int argc, char *argv[]) {
                                 goto finish;
                         }
                         if (r > 0) {
-                                log_full(LOG_CRIT, "Legacy cgroup v1 support selected. This is no longer supported. Will proceed anyway after 30s.");
-                                (void) usleep_safe(30 * USEC_PER_SEC);
+                                log_full(LOG_CRIT, "Legacy cgroup v1 support selected. This is no longer supported. Will proceed anyway after 0s.");
                         }
                 }
 
